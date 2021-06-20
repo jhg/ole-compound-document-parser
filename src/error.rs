e9a2f3ff66c496c21d704d2adfad3f15d2e012b4
Jesus Hernandez <jesushdez@protonmail.com>
2021-06-20 02:12:04 +0200

    refactor: Change code format

--- src/error.rs
+++ src/error.rs
@@ -19,63 +19,63 @@ use std;
 /// Errors related to the process of parsing.
 #[derive(Debug)]
 pub enum Error {
-  /// This happens when filesize is null, or to big to fit into an usize.
-  BadFileSize,
+    /// This happens when filesize is null, or to big to fit into an usize.
+    BadFileSize,
 
-  /// Classic std::io::Error.
-  IOError(std::io::Error),
+    /// Classic std::io::Error.
+    IOError(std::io::Error),
 
-  /// Something is not implemented yet ?
-  NotImplementedYet,
+    /// Something is not implemented yet ?
+    NotImplementedYet,
 
-  /// This is not a valid OLE file.
-  InvalidOLEFile,
+    /// This is not a valid OLE file.
+    InvalidOLEFile,
 
-  /// Something has a bad size.
-  BadSizeValue(&'static str),
+    /// Something has a bad size.
+    BadSizeValue(&'static str),
 
-  /// MSAT is empty.
-  EmptyMasterSectorAllocationTable,
+    /// MSAT is empty.
+    EmptyMasterSectorAllocationTable,
 
-  /// Malformed SAT.
-  NotSectorUsedBySAT,
+    /// Malformed SAT.
+    NotSectorUsedBySAT,
 
-  /// Unknown node type.
-  NodeTypeUnknown,
+    /// Unknown node type.
+    NodeTypeUnknown,
 
-  /// Root storage has a bad size.
-  BadRootStorageSize,
+    /// Root storage has a bad size.
+    BadRootStorageSize,
 
-  /// User query an empty entry
-  EmptyEntry,
+    /// User query an empty entry
+    EmptyEntry,
 }
 
 impl std::error::Error for Error {
-  fn description(&self) -> &str {
-    match *self {
-      Error::BadFileSize => "Filesize is null or too big.",
-      Error::IOError(ref e) => e.description(),
-      Error::NotImplementedYet => "Method not implemented yet",
-      Error::InvalidOLEFile => "Invalid OLE File",
-      Error::BadSizeValue(ref e) => e,
-      Error::EmptyMasterSectorAllocationTable => "MSAT is empty",
-      Error::NotSectorUsedBySAT => "Sector is not a sector used by the SAT.",
-      Error::NodeTypeUnknown => "Unknown node type",
-      Error::BadRootStorageSize => "Bad RootStorage size",
-      Error::EmptyEntry => "Empty entry"
+    fn description(&self) -> &str {
+        match *self {
+            Error::BadFileSize => "Filesize is null or too big.",
+            Error::IOError(ref e) => e.description(),
+            Error::NotImplementedYet => "Method not implemented yet",
+            Error::InvalidOLEFile => "Invalid OLE File",
+            Error::BadSizeValue(ref e) => e,
+            Error::EmptyMasterSectorAllocationTable => "MSAT is empty",
+            Error::NotSectorUsedBySAT => "Sector is not a sector used by the SAT.",
+            Error::NodeTypeUnknown => "Unknown node type",
+            Error::BadRootStorageSize => "Bad RootStorage size",
+            Error::EmptyEntry => "Empty entry"
+        }
     }
-  }
 
-  fn cause(&self) -> Option<&std::error::Error> {
-    match *self {
-      Error::IOError(ref e) => Some(e),
-      _ => None
+    fn cause(&self) -> Option<&dyn std::error::Error> {
+        match *self {
+            Error::IOError(ref e) => Some(e),
+            _ => None
+        }
     }
-  }
 }
 
 impl std::fmt::Display for Error {
-  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-    write!(f, "{}", self.to_string())
-  }
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.to_string())
+    }
 }
